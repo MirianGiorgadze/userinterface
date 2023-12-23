2094bd832cba55b9d9ccd64624c22d6c8194076b
Mirian Giorgadze <m.giorgadze@a1qa.com>
2023-12-24 03:18:05 +0400

    initial commit

---
+++ src/test/java/userinyerface/UserinyerfaceBaseTest.java
@@ -0,0 +1,27 @@
+package userinyerface;
+
+import aquality.selenium.browser.AqualityServices;
+import aquality.selenium.browser.Browser;
+import aquality.selenium.core.utilities.ISettingsFile;
+import aquality.selenium.core.utilities.JsonSettingsFile;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.assertEquals;
+
+public abstract class UserinyerfaceBaseTest {
+    @BeforeMethod
+    public void setUp() {
+        Browser browser = AqualityServices.getBrowser();
+        browser.maximize();
+        ISettingsFile configFile = new JsonSettingsFile("config.json");
+        browser.goTo(String.valueOf(configFile.getValue("/baseUrl")));
+        browser.waitForPageToLoad();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        Browser browser = AqualityServices.getBrowser();
+        browser.quit();
+    }
+}
