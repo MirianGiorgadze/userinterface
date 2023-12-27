03a6bb71997c90b7f7b93481ef9865eaf68d2bcc
Mirian Giorgadze <m.giorgadze@a1qa.com>
2023-12-27 10:39:25 +0400

    empty project

--- src/main/java/userinyerface/utils/ParameterizedXpathUtils.java
+++
@@ -1,9 +0,0 @@
-package userinyerface.utils;
-
-import org.openqa.selenium.By;
-
-public class ParameterizedXpathUtils {
-    public static By getXpathForCheckbox(Integer index) {
-        return By.xpath("//div[contains(@class,'list')][" + index.toString() + "]//span[contains(@class,'checkbox_')]");
-    }
-}
